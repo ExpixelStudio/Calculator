97afb749fd2418f9e08b79fc2c71293015aad074
gtlegend <yusuf_ar@live.com>
2022-08-16 01:05:33 -0400

    Most functionality complete ,TODO code decimal and negative buttons . Style css for these buttons.

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let display = document.querySelector('.display');
     let i = 0;
     let currentValue = ''; 
     let storedValue = [];
-    let storedValue2 = 0;
     let currentOp = '';
     let storedOp = [];
     let result = 0;
     let opResult = 0;
+
+    let maxLength = 13;
 
 
 let add = (a,b) => a+b;
@@ -19,6 +20,7 @@ let subtract = (a,b) => a-b;
 let multiply = (a,b) => a*b;
 
 let divide = (a,b) => a/b;
+
 
 function operate(inputA , op , inputB, ...more) {
     
@@ -40,74 +42,89 @@ function operate(inputA , op , inputB, ...more) {
     }
 }
 
+
 function numberButtons() {
     const numbers = document.querySelectorAll('.num-btn');
-    
-    
-    numbers.forEach((button) => {
-        button.addEventListener('click' , function(e) {
-            display.textContent += button.textContent;
 
-            currentValue += button.textContent; 
-            console.log({currentValue});
+    numbers.forEach((button) => {
+            button.addEventListener('click' , function(e) {
+                
+                if (currentValue.length < maxLength){           // Limits user imput to 13 numbers.
+                    display.textContent += button.textContent;
+                    currentValue += button.textContent;
+                    console.log({currentValue});
+                    } else {
+                        return;
+                }
+                
+                if(i == 1){
+                    display.textContent = currentValue;
+                }
+            console.log(currentValue.length);
         });
     });
 }
+    
+
 
 function operatorButtons() {
    
     
-    const operators = document.querySelectorAll('.operator');
+    const operators = document.querySelectorAll('.operator-btn');
 
     operators.forEach((op) => {
         op.addEventListener('click' , function(e){
-            
-             //for (let i= 0 ; i < 10 ; i++) {
-             
+            if (currentValue == '') {  // if no value entered do nothing (fixes NaN error)
+                return 
+            } 
              storedValue[i] = parseInt(currentValue); //when any operator button is pressed store currentValue in. 
              console.log({storedValue});
 
-             currentValue = ''; //clear first currentValue after storing
+             
+             
+             currentValue = ''; //clear currentValue after storing
+             
             
             if (op.id === 'plus') {
                 storedOp[i] = op.textContent;
-                display.textContent += op.textContent;
+                //display.textContent += op.textContent;
             }
 
             if (op.id === 'minus') {
                 storedOp[i] = op.textContent;
-                display.textContent += op.textContent;
+                //display.textContent += op.textContent;
             }
 
             if (op.id === 'multiply') {
                 storedOp[i] = op.textContent;
-                display.textContent += op.textContent;
+                //display.textContent += op.textContent;
             }
 
             if (op.id === 'divide') {
                 storedOp[i] = op.textContent;
-                display.textContent += op.textContent;
+                //display.textContent += op.textContent;
             }
-            
+        
             console.log({storedOp});
-           //opResult = operate(storedValue , storedOp , currentValue);
             
+        
            i++;
            console.log({i});
-
-           if (i === 2) { // operates on values after 2 values are entered
+        
+           if (i === 2) {  // operates on values after 2 values are entered
             
+           
                 result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
-              // display.textContent = '';
-                display.textContent += "=" + result + storedOp; 
+                let strResult = result.toString(); //converts numerical result to a string
+                let trimResult = strResult.substring(0,maxLength); //trims string to maximum of 13 characters
+                display.textContent = trimResult; //outputs trimmed result to display
                 console.log({result});
                 i = 1;
                 storedValue[0] = result;
-                storedOp[0] = storedOp[1];
-                //storedValue[0] = result ;
-           }
-
-
+                storedOp[0] = storedOp[1]; // set 2nd operator pressed as the current operator after calculating with 1st pressed op.
+            }   else{
+                    return;
+            }     
         });
     });
 }
@@ -120,10 +137,11 @@ function equalButton(){
         storedValue[1] = parseInt(currentValue); //stores new currentValue after pressing = button
         result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
               // display.textContent = '';
-                display.textContent += "=" + result; 
+              let strResult = result.toString(); //converts numerical result to a string
+              let trimResult = strResult.substring(0,maxLength); //trims string to maximum of 13 characters
+                display.textContent = trimResult; //outputs trimmed result to display
                 console.log({result});
-                /* storedValue[0] = result;
-                storedOp[0] = storedOp[1]; */
+                
                 i = 1;
         console.log({result});
     });    
@@ -136,16 +154,36 @@ equalButton();
 
 
 let clearBtn = () => {
-    const clear = document.getElementById('clr') 
+    const clear = document.getElementById('clr');
 
     clear.addEventListener('click' , function(e){
         display.textContent = '';
+        currentValue = '';
+        storedOp[i] = [];
+        storedValue[i] = [];
+         i=0;
     });
 }
 
 clearBtn();
 
 
+let backSpcBtn = () => {
+    let j = 1;
+    const backSpace = document.getElementById('bkspc');
+
+    backSpace.addEventListener('click' , function(e){
+        
+        console.log({j});
+        console.log(currentValue = currentValue.substring(0,currentValue.length-j)); //
+        display.textContent = currentValue;
+        /* if (currentValue.length < 0) {
+            console.log(currentValue.length);
+        }  */
+    });
+}
+
+backSpcBtn();    
 
 
 
