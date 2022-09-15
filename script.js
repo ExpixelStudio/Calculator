aa38441444550b9d040b40f01bb1af08c939a699
gtlegend <yusuf_ar@live.com>
2022-09-15 06:19:38 -0400

    v1.1 refactored alot of code, added color change button, fixed functionality of negative button.

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ let display = document.querySelector('.display');
     let opResult = 0;
     let maxLength = 13;
 
+    let displayValue = '';
+    let state = '';
+
 
  //Calculate Functions
 let add = (a,b) => a+b;
@@ -23,6 +26,14 @@ let divide = (a,b) => a/b;
 
 
 function operate(inputA , op , inputB) {
+    
+    if (op == '÷' && inputB == 0 || inputB == '0'){
+        display.style.fontSize = '1.5vw'; //makes font smaller to support longer text
+        maxLength = 50;
+        displayValue = 'dividing by zero? I wont allow it! {Please Clear}';
+        updateDisplay();
+        return;
+    }
     
     if (op == '+') {
         return add(inputA , inputB);
@@ -41,26 +52,36 @@ function operate(inputA , op , inputB) {
     }
 }
 
+function updateDisplay() {
+    const display = document.querySelector('.display');
+    
+    if (typeof displayValue != 'string') {
+        displayValue = displayValue.toString();
+    } 
+        display.innerText = displayValue;
+        if(displayValue.length > maxLength) {
+        display.innerText = displayValue.substring(0, maxLength);
+    }
+}
+
 
 function numberButtons() {
     const numbers = document.querySelectorAll('.num-btn');
 
     numbers.forEach((button) => {
             button.addEventListener('click' , function(e) {
-                if (currentValue.length < maxLength){           // Limits user imput to 13 numbers.
-
-                    currentValue += button.textContent;
-                    display.textContent = currentValue;
-                    console.log({currentValue});
-                    } else {
-                        return;
-                }
-    
-            console.log(currentValue.length);
+               /* if (state == 'isResult') {
+                    displayValue = '';
+               } */
+                
+                displayValue += button.textContent;
+                updateDisplay();
+                state = 'isInput';
         });
     });
 }
 numberButtons();   
+
 
 
 function operatorButtons() {
@@ -70,16 +91,13 @@ function operatorButtons() {
     operators.forEach((op) => {
         op.addEventListener('click' , function(e){
            
-            if (currentValue == '') {  // if no value entered do nothing (fixes NaN error)
+           
+            if (displayValue == '' ) {  // if no value entered do nothing (fixes NaN error)
                 return; 
-            } //else
-             storedValue[i] = parseFloat(currentValue); //when any operator button is pressed store currentValue in. 
-             console.log({storedValue});
+            } 
 
-
-             currentValue = ''; //clear currentValue after storing
-             
-            
+            state = 'isInput';
+           
             if (op.id === 'plus') {
                 storedOp[i] = op.textContent;
             }
@@ -97,25 +115,44 @@ function operatorButtons() {
             }
         
             console.log({storedOp});
+            console.log({storedValue});
+
+            switch (i){
+                case 0 :
+                    storedValue[0] = parseFloat(displayValue);
+                    console.log('this is nice innit');
+                    displayValue = '';
+                    break;
+
+                case 1 :
+                    storedValue[1] = parseFloat(displayValue);
+                    console.log('that not nice foo');
+                    displayValue = ''
+                    break;
+    
+            }
             
-        
-           i++;
-           console.log({i});
+            i++; // increment i
+            console.log({i});
         
            if (i === 2) {  // operates on values after 2 values are entered
             
-                result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
-                let strResult = result.toString(); //converts numerical result to a string
-                let trimResult = strResult.substring(0,maxLength); //trims string to maximum of 13 characters
-                display.textContent = trimResult; //outputs trimmed result to display
+                result = operate(storedValue[0] , storedOp[0] , storedValue[1]); 
+                
                 console.log({result});
-                i = 1; //sets i to 1 since 0 will be populated with previous result and operator.
-
+            
                 storedValue[0] = result;
+                displayValue = storedValue[0];
                 storedOp[0] = storedOp[1]; // set 2nd operator pressed as the current operator after calculating with 1st pressed op.
-            }   else{
-                    return;
-            }     
+                updateDisplay();
+                state = 'isResult';
+                displayValue = storedValue[0];
+                i = 1; // returns to case 1 for input of 2nd value after storing result as first value.
+                displayValue = ''; // clear for entry on new value
+                console.log({storedValue});
+            }   
+
+        
         });
     });
 }
@@ -127,19 +164,21 @@ function equalButton(){
     const equalBtn = document.getElementById('equals');
     
     equalBtn.addEventListener('click' , function(e){
-        if (currentValue == '') {  // if no value entered do nothing (fixes NaN error)
+        if (displayValue == ''  || storedValue.length < 1 ) {  // if no value entered do nothing (fixes NaN error)
             return ;
         }
-        storedValue[1] = parseFloat(currentValue); //stores new currentValue after pressing = button
+        
+        storedValue[1] = parseFloat(displayValue);
         result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
-              // display.textContent = '';
-              let strResult = result.toString(); //converts numerical result to a string
-              let trimResult = strResult.substring(0,maxLength); //trims string to maximum of 13 characters
-                display.textContent = trimResult; //outputs trimmed result to display
-                console.log({result});
+        
+        storedValue[0] = result; 
+        displayValue = storedValue[0];
+        updateDisplay();       
+        state = 'isResult';
+        i=0;
                 
-                i = 1;
-        console.log({result});
+            console.log({i});
+        
     });    
 }
 equalButton();
@@ -149,11 +188,12 @@ let decimalBtn = () => {
     const decimal = document.getElementById('decimal');
         
     decimal.addEventListener('click' , function(e){
-        if (currentValue.includes('.')) {
+        if (displayValue.includes('.')) {
             return ;
         }
-        currentValue += decimal.textContent;
-        display.textContent = currentValue;
+        displayValue += decimal.textContent;
+        updateDisplay();
+    
     });
 }
 decimalBtn();
@@ -161,18 +201,19 @@ decimalBtn();
 
 let negativeToogle = () => {
     const negative = document.getElementById('negative');
-    const neg = '-';
+    
 
     negative.addEventListener('click' , function(e){
-        if (currentValue == ''){ //Cannot toogle negative if no value entered
-            return;
-        }
-        else if (currentValue.includes(neg)) {                                 //if '-' is already present 
-           currentValue = currentValue.substring(1,currentValue.length)  // return new string starting at 
-           display.textContent = currentValue;
-        }else
-            currentValue = neg.concat(currentValue); //adds '-' to beginning of the currentValue String 
-            display.textContent = currentValue;
+        
+        if (displayValue == '0'  || displayValue == ''){ //Cannot toogle negative if no value entered
+                return;
+        } 
+
+        displayValue = displayValue * -1;
+        updateDisplay();
+        state = 'isResult';
+        
+        console.log('neggy');
     }); 
 }
 negativeToogle();
@@ -182,11 +223,15 @@ let clearBtn = () => {  //Clears screen and resets all values.
     const clear = document.getElementById('clr');
 
     clear.addEventListener('click' , function(e){
+        displayValue = '';
+        updateDisplay();
         display.textContent = '0';
-        currentValue = '';
         storedOp[i] = [];
         storedValue[i] = [];
          i= 0;
+        display.style.fontSize = '2.5vw'; //must change if change value in css
+        maxLength = 13;
+         
     });
 }
 clearBtn();
@@ -197,15 +242,48 @@ let backSpcBtn = () => {
     const backSpace = document.getElementById('bkspc');
 
     backSpace.addEventListener('click' , function(e){
+
+    if (state == 'isResult'){ // prevents backspacing result
+        //DO NOTHING
+    } else {
         
     console.log({j});
-    console.log(currentValue = currentValue.substring(0,currentValue.length-j)); //
-    display.textContent = currentValue;
-
+    console.log(displayValue = displayValue.substring(0,displayValue.length-j));
+    updateDisplay();
+}
     });
 }
 backSpcBtn();    
 
+
+const colorBtn = () => {
+    const changeColor = document.getElementById('color');
+     const top = document.querySelector('.top-panel');
+     const mid = document.querySelector('.display-container');
+     const bottom = document.querySelector('.device-container');
+     let k = 0;
+ 
+     const color = ['rgb(135, 206, 235)' , 'rgb(218, 210, 106)' , 'rgb(209, 127, 80)' , 'rgb(43, 177, 72)' ];    
+                     //skyblue(default), yellow , orange , green
+ 
+     changeColor.addEventListener('click' , function(e){
+         
+         k++;
+         if (k > 3) {
+             k= 0;
+         }
+         top.style.backgroundColor = color[k];
+         mid.style.borderColor = color[k];
+         bottom.style.backgroundColor = color[k];
+         
+         console.log(color);
+     });
+ }
+ colorBtn(); // call colorButton
+
+
+ /* pretty sure not the ideal way to handle the keyboard input, may redo the way i handled functions...
+    so i can call function functionality via click and on keypress */
 
 function keyBoardInput() {
    document.addEventListener('keydown', function(e){
@@ -213,105 +291,134 @@ function keyBoardInput() {
      let keyValue = parseInt(e.key); //converts event.key code to a number
      let j = 1;
      
-    
-    //---BackSpace Key---
-     if(key == 'Backspace') {
-        currentValue = currentValue.substring(0,currentValue.length-j);
-        display.textContent = currentValue;
-        console.log(key);
-    }
-    //___BackSpace Key___
-    
+    switch (key) {
+        //back-space key
+        case 'Backspace' :
+            if (state == 'isResult'){ // prevents backspacing result
+                //DO NOTHING
+            } else {
+            
+            displayValue = displayValue.substring(0,displayValue.length-j);
+            updateDisplay();
+        }
+        break;
+        //back-space key _end
 
-    //---Operator Keys---
-    if (currentValue == ''){
-        void(0); //void(0) also tells JS to do Nothing
-    } else if (key == '+' || key == '-' || key == '*' || key == '/') {
-        storedOp[i] = key;
-        storedValue[i] = parseFloat(currentValue); //when any operator button is pressed store currentValue in
-        currentValue = ''; //clear currentValue after storing
-        i++;
-        console.log({storedOp});
+        //operator keys
+        case '+' :
+        case '-' :
+        case '*' :
+        case '/' :
+            if (displayValue == '' ) {  // if no value entered do nothing (fixes NaN error)
+                return; 
+            } 
+
+            state = 'isInput';
+           
+            storedOp[i] = key;
         
-        if (i === 2) {  // operates on values after 2 values are entered
-            result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
-            let strResult = result.toString(); //converts numerical result to a string
-            let trimResult = strResult.substring(0,maxLength); //trims string to maximum of 13 characters
-            display.textContent = trimResult; //outputs trimmed result to display
-            console.log({result});
-            i = 1;
-            storedValue[0] = result;
-            storedOp[0] = storedOp[1]; // set 2nd operator pressed as the current operator after calculating with 1st pressed op.
-        }   else{
+            switch (i){
+                case 0 :
+                    storedValue[0] = parseFloat(displayValue);
+                    console.log('this is nice innit');
+                    displayValue = '';
+                    break;
+
+                case 1 :
+                    storedValue[1] = parseFloat(displayValue);
+                    console.log('that not nice foo');
+                    displayValue = ''
+                    break;
+            }
+            
+            i++; // increment i
+            console.log({i});
+        
+           if (i === 2) {  // operates on values after 2 values are entered
+            
+                result = operate(storedValue[0] , storedOp[0] , storedValue[1]); 
+                //operate();
+                
+                console.log({result});
+                console.log(storedValue.length);
+                
+
+                storedValue[0] = result;
+                displayValue = storedValue[0];
+                storedOp[0] = storedOp[1]; // set 2nd operator pressed as the current operator after calculating with 1st pressed op.
+                updateDisplay();
+                state = 'isResult';
+                displayValue = storedValue[0];
+                i = 1 ;  // returns to case 1 for input of 2nd value after storing result as first value.
+                displayValue = ''; // clear for entry on new value
+            }   
+            break;
+            //operator keys _end
+
+            //equal key
+            case '=' :
+            case 'Enter' :
+                if (displayValue == ''  || storedValue.length < 1 ) {  // if no value entered do nothing (fixes NaN error)
+                    return ;
+                }
+                
+                storedValue[1] = parseFloat(displayValue);
+                result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
+                
+                storedValue[0] = result; 
+                displayValue = storedValue[0];
+                updateDisplay();       
+                state = 'isResult';
+                i=0;
+            break;
+            //equal key _end
+
+            //decimal key
+            case '.' :
+                if (displayValue.includes('.')) {
+                    return ;
+                }
+                displayValue += decimal.textContent;
+                updateDisplay();
+            break;
+
+            case '`' :
+                if (displayValue == '0'  || displayValue == ''){ //Cannot toogle negative if no value entered
+                    return;
+                } 
+
+                displayValue = displayValue * -1;
+                updateDisplay();
+                state = 'isResult';
+            break;
+            //decimal key _end
+
+            //clear key
+            case 'Delete' :
+                displayValue = '';
+                updateDisplay();
+                display.textContent = '0';
+                storedOp[i] = [];
+                storedValue[i] = [];
+                i= 0;
+                display.style.fontSize = '2.5vw'; //must change if change value in css
+                maxLength = 13;
+            break;
+            //clear key _end
+            
+        default :
+        //number keys
+            if(Number.isNaN(keyValue)) { //if keyValue IS NOT a number do nothing.
                 return;
-        }     
+            } 
+            if (state == 'isResult') {
+                displayValue = '';
+            } 
+            displayValue += keyValue;
+            updateDisplay();
+            state = 'isInput';
+            
     }
-    //___Operator Keys___
-     
-    
-    //---Number Keys---
-    if(Number.isNaN(keyValue)) { //if keyValue IS NOT a number do nothing.
-        //DO NOTHING
-    } else if(currentValue.length < maxLength) {
-        currentValue += keyValue;
-        display.textContent = currentValue;
-        console.log(key);
-    }
-    //___Number Keys___
-
-
-    //---Equal Key---
-    if(key == '=' || key == 'Enter') {
-        if (currentValue == '') {  // if no value entered do nothing (fixes NaN error)
-            return ;
-        }
-
-        storedValue[1] = parseFloat(currentValue); //stores new currentValue after pressing = button
-        result = operate(storedValue[0] , storedOp[0] , storedValue[1]);
-            // display.textContent = '';
-        let strResult = result.toString(); //converts numerical result to a string
-        let trimResult = strResult.substring(0,maxLength); //trims string to maximum of 13 characters
-        display.textContent = trimResult; //outputs trimmed result to display
-        console.log({result});  
-        i = 1;
-            console.log({result});
-    } 
-    //___Equal Key___
-
-    //---Decimal Key---
-    if(key== '.') {
-        if (currentValue.includes('.')) {
-            return;
-        }
-        currentValue += key;
-        display.textContent = currentValue;
-    }
-    //___Decimal Key___
-
-    //--Negative Key---
-    if (key == '`') {
-        if (currentValue == ''){ //Cannot toogle negative if no value entered
-            return;
-        }
-        else if (currentValue.includes('-')) {                                 //if '-' is already present 
-           currentValue = currentValue.substring(1,currentValue.length)  // return new string starting at 
-           display.textContent = currentValue;
-        }else
-            currentValue = '-'.concat(currentValue); //adds '-' to beginning of the currentValue String 
-            display.textContent = currentValue;
-    }
-
-    //---Clear Key---
-    if(key == 'Delete') {
-        display.textContent = '0';
-        currentValue = '';
-        storedOp[i] = [];
-        storedValue[i] = [];
-        i= 0;
-    }
-    //___Clear Key___
-    
    });
 }
 keyBoardInput();
-
